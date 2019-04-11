e2acdac3459aa415e9d9889f67cb360a25a14a28
David Latorre <david@latorredev.com>
2019-04-11 18:21:59 -0500

    [Feature] 1 add basic structure to main, printf inside a infinite loop

---
+++ micro_shell.c
@@ -0,0 +1,26 @@
+#include "microshell.h"
+
+/* - PID
+*
+* Return: Always 0.
+*/
+
+int main (void)
+{
+
+	char *buffer = NULL;
+	size_t len = 0;
+
+	while (1)
+	{
+		printf("$ ");
+
+	}
+
+	return (0);
+}
+
+ssize_t _getline (char **lineptr, size_t *n, stdin)
+{
+
+}
