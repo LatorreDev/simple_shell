3452c1dcd8cb6404fb52346d4c6afd303dc19068
David Latorre <david@latorredev.com>
2019-04-11 18:21:46 -0500

    [Feature] 1 add libraries neccesary to the project

---
+++ microshell.h
@@ -0,0 +1,17 @@
+#ifndef _MICROSHELL_H_
+#define _MICROSHELL_H_
+
+/* Libraries */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <strings.h>
+#include <limits.h>
+#include <signal.h>
+
+/* Prototypes */
+
+#endif /* _MICROSHELL_H_ */
