41df40069d7d411911286be91ae2df6903bcb03f
David Latorre <david@latorredev.com>
2019-04-14 16:46:29 -0500

    [Feature] add betty style

--- micro_shell.c
+++ micro_shell.c
@@ -1,51 +1,49 @@
 #include "microshell.h"
 
-/*
-*
+/**
+* main - entry point of the program
+* Return: return 0 if sucess
 */
 
-int main (void)
+int main(void)
 {
 	char *buffer = NULL;
-	size_t len = 0; 
+	size_t len = 0;
 	int status;
-	char *argv[] = {"",NULL};
+	char *argv[] = {"", NULL};
 	char *token;
 	int another_status;
 	int valid_exec;
 
 	while (1)
 	{
-		write(1, "$ ",2);
+		write(1, "$ ", 2);
 		buffer = NULL;
-		status = getline (&buffer, &len, stdin);
+		status = getline(&buffer, &len, stdin);
 
 		if (status == -1)
 		{
-			perror ("./shell");
+			perror("./shell");
 			free(buffer);
 			exit(-1);
 		}
 		token = strtok(buffer, " \n\t");
 
-		if(fork() == 0)
+		if (fork() == 0)
 		{
 		valid_exec = execve(token, argv, NULL);
 			if (valid_exec == -1)
 			{
-				perror ("./shell");
+				perror("./shell");
 				free(buffer);
-				exit (-1);
+				exit(-1);
 			}
-
 		}
-
 		else
 		{
-			wait (&another_status);
+			wait(&another_status);
 		}
 		free(buffer);
 	}
 	return (0);
-
 }
