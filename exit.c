97061cdc70c2e185f33b554716a93b810c1ab958
David Latorre <david@latorredev.com>
2019-04-16 17:36:29 -0500

    [Fix] fix betty coding style

--- exit.c
+++ exit.c
@@ -6,11 +6,11 @@
 *  Return: 0 if is sucess.
 */
 
-int exit_func (char *my_str)
+int exit_func(char *my_str)
 {
-	if (_strcmp (my_str, "exit") == 0)
+	if (_strcmp(my_str, "exit") == 0)
 	{
-		exit (0);
+		exit(0);
 	}
 	return (0);
 }
